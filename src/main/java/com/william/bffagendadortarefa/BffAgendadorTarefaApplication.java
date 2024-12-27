bd8291063a642a47348394b15c56d8ff72ec7b2a
wkoju84 <wkoju84@gmail.com>
2024-12-27 12:35:51 -0300

    adicionado esqueleto do projeto

---
+++ src/main/java/com/william/bffagendadortarefa/BffAgendadorTarefaApplication.java
@@ -0,0 +1,13 @@
+package com.william.bffagendadortarefa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffAgendadorTarefaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffAgendadorTarefaApplication.class, args);
+	}
+
+}
